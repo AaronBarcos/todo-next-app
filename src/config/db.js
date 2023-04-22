7e65e0180a71bf847b7b7415d674ad8f0e42ee2d
AaronBarcos <abarcoscaballero@gmail.com>
2023-04-22 14:22:32 +0200

    change

--- src/config/db.js
+++ src/config/db.js
@@ -1,41 +1,35 @@
 import mongoose from "mongoose";
 
-const cors = require("cors");
-app.use(cors());
-
 const DB = process.env.MONGODB_URI;
 
 if (!DB) {
-	throw new Error(
-		"Define environment variable inside .env.local"
-	);
+  throw new Error("Define environment variable inside .env.local");
 }
 
 let cached = global.mongoose;
 
 if (!cached) {
-	cached = global.mongoose = { connection: null, promise: null };
+  cached = global.mongoose = { connection: null, promise: null };
 }
 
 const dbConnect = async () => {
-	if (cached.connection) {
-		return cached.connection;
-	}
 
-	if (!cached.promise) {
-		const options = {
-			useNewUrlParser: true,
-			useUnifiedTopology: true,
-		};
+  if (cached.connection) {
+    return cached.connection;
+  }
 
-		cached.promise = mongoose.connect(DB, options).then((mongoose) => {
-			return mongoose;
-		});
-	}
-	cached.connection = await cached.promise;
-	return cached.connection;
+  if (!cached.promise) {
+    const options = {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    };
+
+    cached.promise = mongoose.connect(DB, options).then((mongoose) => {
+      return mongoose;
+    });
+  }
+  cached.connection = await cached.promise;
+  return cached.connection;
 };
 
 export default dbConnect;
-
-
