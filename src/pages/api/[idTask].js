5af2e989c6fa89190dc97de7ac52564048e78287
AaronBarcos <abarcoscaballero@gmail.com>
2023-04-24 20:10:11 +0200

    cors

--- src/pages/api/[idTask].js
+++ src/pages/api/[idTask].js
@@ -1,7 +1,15 @@
 import dbConnect from "../../config/db";
 import Task from "../../models/task";
+import NextCors from "nextjs-cors";
 
 export default async function handler(req, res) {
+
+  await NextCors(req, res, {
+    methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
+    origin: "*",
+    optionsSuccessStatus: 200,
+  });
+
   const { method } = req;
   const { idTask } = req.query;
 
